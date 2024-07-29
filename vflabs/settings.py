734b4e116eb87e430eb4824ee490468a8c32f41d
azaria-morake <azariamorake@gmail.com>
2024-07-29 16:40:49 +0200

    updated codebase

--- vflabs/settings.py
+++ vflabs/settings.py
@@ -1,25 +1,4 @@
 from pathlib import Path
-import environ
-import os
-import dj_database_url
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
-
-# Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-# False if not in os.environ because of casting above
-DEBUG = env('DEBUG')
-
-# Raises Django's ImproperlyConfigured
-# exception if SECRET_KEY not in os.environ
-SECRET_KEY = env('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,7 +13,7 @@ SECRET_KEY = 'django-insecure-*wq604lwk^iqx5iq&81#0bwrxz$gt__ho^5zgjf0p7x+%mui=%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vf-labs.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -84,7 +63,10 @@ WSGI_APPLICATION = 'vflabs.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -122,13 +104,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     #BASE_DIR / "static",
     BASE_DIR / 'vflabs_home/static',
-    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
